07ff6c253d27ddc999035b81dd037899586546cc
Michael Rochelle <EliteScientist@users.noreply.github.com>
2020-01-23 17:22:41 -0800

    Polymer 3 Port (#8)
    * Ported loader to work with Polymer 3.
    
    * Updated version #
    
    * Added try catch to prevent breaking the application in the case of a component failure.
    
    * Fixed issue with CSS not being wrapped in the style tag.
    Changed the static template function to a getter to mimic Polymer's documentation.
    
    * update dependencies.

--- index.js
+++ index.js
@@ -1,80 +1,68 @@
 
 'use strict';
 
-var path = require('path');
-var fs = require('fs');
-var loaderUtils = require('loader-utils');
-var SourceMap = require('source-map');
+const path 			= require('path');
+const fs 			= require('fs');
+const loaderUtils 	= require('loader-utils');
+const SourceMap 	= require('source-map');
 
-module.exports = function(source, sourceMap) {
+module.exports = function (source, sourceMap)
+{
+    let query = loaderUtils.parseQuery(this.query);
 
-    var query = loaderUtils.parseQuery(this.query);
-
-    if(this.cacheable) {
+    if (this.cacheable)
         this.cacheable();
-    }
 
     // /foo/bar/file.js
-    var srcFilepath = this.resourcePath;
+    let srcFilepath 	= this.resourcePath;
+	
     // /foo/bar/file.js -> file
-    var srcFilename = path.basename(srcFilepath, path.extname(srcFilepath));
+    let srcFilename 		= path.basename(srcFilepath, path.extname(srcFilepath));
+	
     // /foo/bar/file.js -> /foo/bar
-    var srcDirpath  = path.dirname(srcFilepath);
+    let srcDirpath  		= path.dirname(srcFilepath);
+	
     // /foo/bar -> bar
-    var srcDirname  = srcDirpath.split(path.sep).pop();
+    let srcDirname  		= srcDirpath.split(path.sep).pop();
 
-    var elementName = srcFilename == 'index' ? srcDirname : srcFilename;
+    let elementName 		= srcFilename == 'index' ? srcDirname : srcFilename;
 
-    var templateExtension = query.templateExt || query.templateExtension || 'html';
-    var styleExtension    = query.styleExt || query.styleExtension || 'css';
+    let templateExtension 	= query.templateExt || query.templateExtension || 'html';
+    let styleExtension    	= query.styleExt || query.styleExtension || 'css';
 
-    var htmlExists = fs.existsSync(path.join(srcDirpath, elementName+'.'+templateExtension));
-    var cssExists  = fs.existsSync(path.join(srcDirpath, elementName+'.'+styleExtension));
+    let htmlExists = fs.existsSync(path.join(srcDirpath, elementName+'.'+templateExtension));
+    let cssExists  = fs.existsSync(path.join(srcDirpath, elementName+'.'+styleExtension));
 
-    var inject = (htmlExists || cssExists) ? '\n/* inject from polymer-loader */\n' : '';
-
-    if(htmlExists & cssExists) {
-        inject += [
-            "(function(document) {",
-                "\tvar template = require('./"+elementName+"."+templateExtension+"');",
-                "\tvar styles = require('./"+elementName+"."+styleExtension+"');",
-                "\tvar el = document.createElement('div');",
-                "\tel.setAttribute('name', '"+elementName+"')",
-                "\tel.setAttribute('hidden','')",
-                "\tif(template.indexOf('<template>'))",
-                "\t\tel.innerHTML = template.replace(/(<template>)([^]*<\\/template>)/img, function(m, $1, $2) { return $1 + '<style>'+styles+'</style>' + $2});",
-                "\telse",
-                "\t\tel.innerHTML = template.replace('</dom-module>', '<template><style>'+styles+'</style></template></dom-module>');",
-                "\tdocument.body.appendChild(el);",
-            "})(document);"
-        ].join('\n');
-    }
-    else
-    if(htmlExists && !cssExists) {
-        inject += [
-            "(function(document) {",
-                "\tvar template = require('./"+elementName+"."+templateExtension+"');",
-                "\tvar el = document.createElement('div');",
-                "\tel.setAttribute('name', '"+elementName+"')",
-                "\tel.setAttribute('hidden','')",
-                "\tel.innerHTML = template;",
-                "\tdocument.body.appendChild(el);",
-            "})(document);"
-        ].join('\n');
-    }
-    else
-    if(!htmlExists && cssExists) {
-        inject += [
-            "(function(document) {",
-                "\tvar styles = require('./"+elementName+"."+styleExtension+"');",
-                "\tvar el = document.createElement('div');",
-                "\tel.setAttribute('name', '"+elementName+"')",
-                "\tel.setAttribute('hidden','')",
-                "\tel.innerHTML = '<dom-module id=\""+elementName+"\"><template><style>'+styles+'</style></template></dom-module>';",
-                "\tdocument.body.appendChild(el);",
-            "})(document);"
-        ].join('\n');
-    }
+    let buffer = (htmlExists || cssExists) ? ['\n/* inject from polymer-loader */\n'] : '';
+	
+	buffer.push("(function() {");
+	buffer.push(	"\tlet componentTemplate	= \"\";");
+	
+	if (cssExists)
+		buffer.push("\tcomponentTemplate +='<style>' + require('./"+elementName+"."+styleExtension+"') + '</style>\\n';");
+		
+	if (htmlExists)
+		buffer.push("\tcomponentTemplate += require('./"+elementName+"."+templateExtension+"') + '\\n';");
+		
+	buffer	= buffer.concat([
+		"\ttry",
+		"\t{",
+			"\t\tlet html = require(\"@polymer/polymer\").html;",
+			"\t\tlet Component 	= require('./"+elementName+".js');",
+			"\t\tif (\"default\" in Component)",
+			"\t\t\tComponent = Component.default;",
+			"\t\tObject.defineProperty(Component, \"_template\", {value: html([componentTemplate]), writable: false, configurable: false});",
+			"\t\tObject.defineProperty(Component, \"template\", {get: function () { return this._template}, configurable: true, enumerable: false});",
+			"\t\tcustomElements.define(Component.is || \"" + elementName + "\", Component);",
+		"\t}",
+		"\tcatch (error)",
+		"\t{",
+			"\t\tconsole.error(error);",
+		"\t}",
+		"})();"
+	]);
+	
+	let inject = buffer.join("\n");
 
     // support existing SourceMap
     // https://github.com/mozilla/source-map#sourcenode
@@ -111,4 +99,3 @@ module.exports = function(source, sourceMap) {
     // return the original source
     return source;
 };
-
